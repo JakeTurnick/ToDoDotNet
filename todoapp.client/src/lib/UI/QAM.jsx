e3d964e159992435c5b51874ab90deb18fb73661
Jake <jaketurnick@gmail.com>
2025-01-27 12:42:22 -0500

    QAM menu styling improvements, transition looks nice

--- todoapp.client/src/lib/UI/QAM.jsx
+++ todoapp.client/src/lib/UI/QAM.jsx
@@ -2,22 +2,26 @@ import styles from "./QAM.module.css";
 import { useEffect, useState } from "react";
 import { Link } from "react-router";
 import QAMItem from "./QAM_Item";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
 export default function QAM() {
     const [isOpen, setIsOpen] = useState(false);
+
+    console.log("QAM isOpen ", isOpen)
 
     return (
         <section className={`${styles.QAM_Container} ${isOpen ? styles.open : styles.closed}`}>
             <article className={styles.QAM_Header}>
                 <FontAwesomeIcon icon="fa-solid fa-user" />
-                {isOpen ? <p>User name</p> : "" }
+                <p className={`${isOpen ? styles.open : styles.closed}`}>User name</p>
                 <button onClick={() => { setIsOpen(!isOpen) }}>
                     <FontAwesomeIcon icon="fa-solid fa-chevron-right" className={`${styles.QAM_Access_btn} ${isOpen ? styles.open : styles.closed}`} />
                 </button>
             </article>
             <nav className={styles.QAM_Nav}>
-                <QAMItem text={"Home"} link={"/Home"} isOpen icon={<FontAwesomeIcon icon="fa-solid fa-house" className={styles.QAM_Item_Icon } />}></QAMItem>
-                <QAMItem text={"To Do's"} link={"/ToDos"} isOpen icon={<FontAwesomeIcon icon="fa-solid fa-list-check" className={styles.QAM_Item_Icon} /> }></QAMItem>
+                <QAMItem text={"Home"} link={"/Home"} isOpen={isOpen} icon={<FontAwesomeIcon icon="fa-solid fa-house" className={styles.QAM_Item_Icon } />}></QAMItem>
+                <QAMItem text={"To Do's"} link={"/ToDos"} isOpen={isOpen} icon={<FontAwesomeIcon icon="fa-solid fa-list-check" className={styles.QAM_Item_Icon} /> }></QAMItem>
             </nav>
         </section>
     )
--- todoapp.client/src/lib/UI/QAM_Item.jsx
+++ todoapp.client/src/lib/UI/QAM_Item.jsx
@@ -3,12 +3,14 @@ import { NavLink } from "react-router";
 import { useEffect, useState } from "react";
 
 export default function QAMItem({ link, text, icon, isOpen }) {
-
+    console.log("QAMIem isOpen ", isOpen)
     
     return (
         <div>
-            <NavLink to={link} className={styles.QAM_Item_Body } >
-                <button className={styles.QAM_Item_btn}>{icon} {isOpen ? text : ""}</button>
+            <NavLink to={link} className={`${styles.QAM_Item_Body}`} >
+                <button className={`${styles.QAM_Item_btn}`}>
+                    {icon} <p className={`${isOpen ? styles.open : styles.closed}`}>{text}</p>
+                </button>
             </NavLink>
         </div>
     )
