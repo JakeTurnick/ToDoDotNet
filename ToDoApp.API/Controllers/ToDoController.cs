99812004c47a6b6e4094a6ca94961dff9aba597a
Jake <jaketurnick@gmail.com>
2025-02-19 10:49:27 -0500

    remaining CRUD enpoints for ToDos

--- ToDoApp.API/Controllers/ToDoController.cs
+++ ToDoApp.API/Controllers/ToDoController.cs
@@ -18,15 +18,35 @@ namespace ToDoApp.API.Controllers
             _dbContext = dbContext;
         }
         [HttpGet]
-        public ActionResult<List<ToDo>> GetToDos()
+        public IActionResult GetToDos()
         {
             var toDos = _dbContext.ToDos.FromSql($"EXECUTE dbo.SelectAllToDos").ToList<ToDo>();
-            return toDos;
+            return Ok(toDos);
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult GetToDoById(int Id)
+        {
+            if (Id <= 0)
+            {
+                return BadRequest("No Id found on request");
+            }
+
+            var id = new SqlParameter("@Id", Id);
+            var result = _dbContext.ToDos.FromSqlRaw($"EXECUTE dbo.SelectToDoById " +
+                $"@Id",
+                id);
+
+            return Ok(result);
         }
 
         [HttpPost]
-        public ActionResult<List<ToDo>> PostToDo(ToDo toDo)
+        public IActionResult PostToDo(ToDo toDo)
         {
+            if (toDo.Name?.Length < 1)
+            {
+                return BadRequest("ToDo name must be present and longer than 1 character");
+            }
             var name = new SqlParameter("@Name", toDo.Name);
             var description = new SqlParameter("@Description", toDo.Description);
             var startDate = new SqlParameter("@StartDate", toDo.StartDate.HasValue ? (object)toDo.StartDate.Value : DBNull.Value);
@@ -44,7 +64,56 @@ namespace ToDoApp.API.Controllers
                 startDate,
                 endDate).ToList<ToDo>();
 
-            return result;
+            return Ok(result);
+        }
+
+        [HttpPatch]
+        public IActionResult UpdateToDo(ToDo toDo)
+        {
+            if (toDo?.Id <= 0 || toDo?.Id == null)
+            {
+                return BadRequest("No toDo Id found on request");
+            }
+            var id = new SqlParameter("@Id", toDo.Id);
+            var name = new SqlParameter("@Name", toDo.Name);
+            var description = new SqlParameter("@Description", toDo.Description);
+            var startDate = new SqlParameter("@StartDate", toDo.StartDate.HasValue ? (object)toDo.StartDate.Value : DBNull.Value);
+            var endDate = new SqlParameter("@EndDate", toDo.EndDate.HasValue ? (object)toDo.EndDate.Value : DBNull.Value);
+            var isCompleted = new SqlParameter("@IsCompleted", toDo.IsCompleted);
+            var result = _dbContext.ToDos.FromSqlRaw($"EXECUTE dbo.UpdateToDo " +
+                $"@Id, " +
+                $"@Name, " +
+                $"@Description, " +
+                $"@IsCompleted, " +
+                $"@StartDate, " +
+                $"@EndDate ",
+                id,
+                name,
+                description,
+                isCompleted,
+                startDate,
+                endDate).ToList<ToDo>();
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{Id}")]
+        public IActionResult DeleteToDoById(int Id)
+        {
+            if (Id <= 0)
+            {
+                return BadRequest("No Id found on request");
+            }
+
+            var id = new SqlParameter("@Id", Id);
+            var result = _dbContext.ToDos.FromSqlRaw($"EXECUTE dbo.DeleteToDoById " +
+                $"@Id",
+                id);
+
+
+            // Works 100% of the time - need to have SQL send something back
+            // Check results of sql
+            return Ok(result);
         }
     }
 }
